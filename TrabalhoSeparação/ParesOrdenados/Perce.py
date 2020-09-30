9f2c4ee355b0e0575e8c55b3e572e42ebd13dc5b
Diego Braga <diego.cstbraga@gmail.com>
2020-09-30 16:57:54 -0300

    Etapa 4 pronta

--- TrabalhoSeparação/ParesOrdenados/Perce.py
+++ TrabalhoSeparação/ParesOrdenados/Perce.py
@@ -1,10 +1,10 @@
-x1 = ([-1, -1], [0.1, 0.1], [0.1, 0.5], [0.3, 0.3])
+x1 = ([-1, -1], [0.28, 0.676], [0.2652, 0.688], [0.22, 0.64])
 
-x2 = ([-1, -1], [0.6, 0.6], [0.8, 0.2], [0.9, 0.5])
+x2 = ([-1, -1], [0.338, 0.7], [0.36, 0.712], [0.42, 0.708])
 
 funcAtivacao, limiar, u0 = 0.0, 0.0, 0.0
-f, grupo0, grupo1 = 0, 0, 1
-taxaAprendizado = 0.1
+f, classe0, classe1 = 0, 0, 1
+taxaAprendizado = 0.13
 
 
 def interacaoPerceptron(w):
@@ -22,7 +22,7 @@ def interacaoPerceptron(w):
 def entradaX1(w, u):
     #  print("----------------------------------\n")
     #  print("Entrada X1")
-    u0 = 0
+    u0 = 1
 
     u0 = w[0] * x1[0][0] + w[1] * x1[u][0] + w[2] * x1[u][1]
     #  print("u0 = %s" % {u0})
@@ -32,11 +32,11 @@ def entradaX1(w, u):
     else:
         f = 0
 
-    w[0] = w[0] + taxaAprendizado * (grupo0 - f) * x1[0][0]
+    w[0] = w[0] + taxaAprendizado * (classe0 - f) * x1[0][0]
     #  print("Pesos w = %s" % {w[0]})
-    w[1] = w[1] + taxaAprendizado * (grupo0 - f) * x1[u][0]
+    w[1] = w[1] + taxaAprendizado * (classe0 - f) * x1[u][0]
     #  print("Pesos w = %s" % {w[1]})
-    w[2] = w[2] + taxaAprendizado * (grupo0 - f) * x1[u][1]
+    w[2] = w[2] + taxaAprendizado * (classe0 - f) * x1[u][1]
     #  print("Pesos w = %s" % {w[2]})
 
     return w
@@ -56,11 +56,11 @@ def entradaX2(w, u):
 
     #  print(f"Valor de f =\t{f}")
 
-    w[0] = w[0] + taxaAprendizado * (grupo1 - f) * x2[0][0]
+    w[0] = w[0] + taxaAprendizado * (classe1 - f) * x2[0][0]
     #  print("Pesos w = %s" % {w[0]})
-    w[1] = w[1] + taxaAprendizado * (grupo1 - f) * x2[u][0]
+    w[1] = w[1] + taxaAprendizado * (classe1 - f) * x2[u][0]
     #  print("Pesos w = %s" % {w[1]})
-    w[2] = w[2] + taxaAprendizado * (grupo1 - f) * x2[u][1]
+    w[2] = w[2] + taxaAprendizado * (classe1 - f) * x2[u][1]
     #  print("Pesos w = %s" % {w[2]})
 
     return w
@@ -72,7 +72,7 @@ def verificaPerceptron(w, x1, x2):
         print("\nPesos resultante do treinamento")
         print("w[" + str(i) + "] = " + str(w[i]))
 
-    u0 = 0
+    u0 = 1
     for p in range(1, 4):
         u0 = w[0] * x1[0][0] + w[1] * x1[p][0] + w[2] * x1[p][1]
         print("\nu0 = " + str(u0))
